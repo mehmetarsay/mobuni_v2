57b2a7a6b4df87a95afccb480bfe0a6c8b286850
mehmetarsay <mehmetarsay5@gmail.com>
2022-05-15 19:46:07 +0300

    app router birleştirildi

--- lib/app/app.router.dart
+++ lib/app/app.router.dart
@@ -11,6 +11,7 @@ import 'package:stacked/stacked.dart';
 import 'package:stacked/stacked_annotations.dart';
 
 import '../feature/views/auth/login/login_view.dart';
+import '../feature/views/auth/register/register_view.dart';
 import '../feature/views/home/bottomnav_view.dart';
 import '../feature/views/question/subviews/question_add/question_add_view.dart';
 import '../feature/views/splash/view/splash_view.dart';
@@ -19,11 +20,13 @@ class Routes {
   static const String splashView = '/';
   static const String bottomNavView = '/bottom-nav-view';
   static const String loginView = '/login-view';
+  static const String registerView = '/register-view';
   static const String questionAddView = '/question-add-view';
   static const all = <String>{
     splashView,
     bottomNavView,
     loginView,
+    registerView,
     questionAddView,
   };
 }
@@ -35,6 +38,7 @@ class StackedRouter extends RouterBase {
     RouteDef(Routes.splashView, page: SplashView),
     RouteDef(Routes.bottomNavView, page: BottomNavView),
     RouteDef(Routes.loginView, page: LoginView),
+    RouteDef(Routes.registerView, page: RegisterView),
     RouteDef(Routes.questionAddView, page: QuestionAddView),
   ];
   @override
@@ -55,6 +59,12 @@ class StackedRouter extends RouterBase {
     LoginView: (data) {
       return MaterialPageRoute<dynamic>(
         builder: (context) => const LoginView(),
+        settings: data,
+      );
+    },
+    RegisterView: (data) {
+      return MaterialPageRoute<dynamic>(
+        builder: (context) => const RegisterView(),
         settings: data,
       );
     },

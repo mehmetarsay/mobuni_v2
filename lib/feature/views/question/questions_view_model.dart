66807b7a427649d5ce03ce2921b098bf1bd9a9b8
mehmetarsay <mehmetarsay5@gmail.com>
2022-05-18 02:35:16 +0300

    firebase bağlantısı yapıldı

--- lib/feature/views/question/questions_view_model.dart
+++ lib/feature/views/question/questions_view_model.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/animation.dart';
 import 'package:flutter/material.dart';
+import 'package:mobuni_v2/feature/models/questions/question_model.dart';
 import 'package:mobuni_v2/feature/views/question/service/question_service.dart';
 import 'package:stacked/stacked.dart';
 
@@ -8,6 +9,8 @@ import '../../../app/app.locator.dart';
 class QuestionsViewModel extends BaseViewModel {
   QuestionService questionService = locator<QuestionService>();
   var indicator = new GlobalKey<RefreshIndicatorState>();
+  ScrollController scrollController = ScrollController();
+
   int _newQuestionSize = 0;
 
   int get newQuestionSize => _newQuestionSize;
@@ -17,23 +20,32 @@ class QuestionsViewModel extends BaseViewModel {
     notifyListeners();
   }
 
-  init()async{
+  List<QuestionModel>? questions;
+
+  init() async {
+    setInitialised(false);
     newQuestionCalculate();
-   await questionService.questionGetByUniversityId(universityId: 1);
+    await getData();
+    setInitialised(true);
     notifyListeners();
   }
 
-  newQuestionCalculate()async{
+  newQuestionCalculate() async {
     ///TODO buraya güncel data sayısı gönderilecek
     newQuestionSize = 3;
   }
-  onTapNewQuestions()async{
+
+  onTapNewQuestions() async {
     newQuestionSize = 0;
-    questionService.scrollController.animateTo(-50,
-        duration: const Duration(seconds: 1), curve: Curves.linear).then((value) {
+    await getData();
+    scrollController.animateTo(-50, duration: const Duration(seconds: 1), curve: Curves.linear).then((value) {
       indicator.currentState!.show();
     });
     notifyListeners();
   }
 
+  Future getData() async {
+    questions = await questionService.questionGetByUniversityId(universityId: 1);
+    notifyListeners();
+  }
 }

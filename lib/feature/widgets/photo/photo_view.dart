ac0e0d9ce9de00b03ef2ba22710bf2aeb81ad29e
mehmetarsay <mehmetarsay5@gmail.com>
2022-05-22 03:34:24 +0300

    fotoğraf büyük ekranda görme yapıldı

--- lib/feature/widgets/photo/photo_view.dart
+++ lib/feature/widgets/photo/photo_view.dart
@@ -1,6 +1,9 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:dismissible_page/dismissible_page.dart';
 import 'package:flutter/material.dart';
 import 'package:mobuni_v2/core/extension/context_extension.dart';
 import 'package:photo_view/photo_view.dart';
+import 'package:shimmer/shimmer.dart';
 
 class CustomPhotoView extends StatefulWidget {
    CustomPhotoView({Key? key,required this.imageUrl}) : super(key: key);
@@ -11,40 +14,52 @@ class CustomPhotoView extends StatefulWidget {
 }
 
 class _CustomPhotoViewState extends State<CustomPhotoView> {
-  ScrollController controller = ScrollController();
-
 
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(),
-      body: RefreshIndicator(
-        edgeOffset: 20,
-          notificationPredicate:(val){
-            print(val.metrics.pixels);
-          if(val.metrics.pixels<15){
-             context.navigationService.back();
-          }
-          return true;
-          },
-        onRefresh: () async{},
-        child: SingleChildScrollView(
-          physics: AlwaysScrollableScrollPhysics(),
-          controller: controller,
-          child: Column(
-            children: [
-              InteractiveViewer(
-                child:Container(
-                    height: context.height+50,
-                    width: context.width,
-                    child: Image.network(
-                      widget.imageUrl,
-                      fit: BoxFit.contain,
-                    )
+    return DismissiblePage(
+      onDismissed: () {
+        context.navigationService.back();
+      },
+      direction: DismissiblePageDismissDirection.down,
+      isFullScreen: true,
+      reverseDuration: Duration(milliseconds: 1),
+      child: Hero(
+        tag: 'Unique tag',
+        child: Container(
+          height: context.height,
+          width: context.width,
+          child: InteractiveViewer(
+              child: CachedNetworkImage(
+                imageUrl: widget.imageUrl,
+                imageBuilder: (context, imageProvider) => Container(
+                  height: context.height,
+                  width: context.width,
+                  alignment: Alignment.center,
+                  decoration: BoxDecoration(
+                    image: DecorationImage(image: imageProvider, fit: BoxFit.contain),
+                  ),
+                ),
+                placeholder: (context, url) => Shimmer.fromColors(
+                  highlightColor: context.theme.primaryColorLight.withOpacity(0.1),
+                  baseColor: context.theme.primaryColorDark.withOpacity(0.1),
+                  child: Container(
+                    height: context.height ,
+                    width: context.width ,
+                    decoration: BoxDecoration(shape: BoxShape.rectangle, borderRadius: BorderRadius.all(Radius.circular(10)), color: Colors.grey),
+                  ),
+                ),
+                errorWidget: (context, url, error) => Container(
+                  height: context.height ,
+                  width: context.width ,
+                  decoration: BoxDecoration(
+                      shape: BoxShape.rectangle,
+                      borderRadius: BorderRadius.all(Radius.circular(10)),
+                      color: context.theme.primaryColorDark.withOpacity(0.1)),
+                  child: Icon(Icons.error),
                 ),
               )
-            ],
           ),
         ),
       ),

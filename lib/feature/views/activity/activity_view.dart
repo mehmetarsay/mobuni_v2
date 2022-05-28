df1e9ed894a021504dd9e2cd0c8c5ac5030db8fd
Bilal Özcan <bilalozcan@bixos.com>
2022-05-28 21:29:10 +0300

    Etkinlikler için hive'a kaydetme yapıldı. Etkinlikler için sayfalama yapıldı.

--- lib/feature/views/activity/activity_view.dart
+++ lib/feature/views/activity/activity_view.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 import 'package:mobuni_v2/core/components/app_bar/custom_app_bar.dart';
+import 'package:mobuni_v2/core/constants/enum/hive_enum.dart';
 import 'package:mobuni_v2/core/extension/context_extension.dart';
 import 'package:mobuni_v2/feature/views/activity/activity_add/activity_add_view.dart';
 import 'package:mobuni_v2/feature/views/activity/activity_view_model.dart';
@@ -23,25 +25,62 @@ class ActivityView extends StatelessWidget {
       appBar: CustomAppBar(title: 'Etkinlikler'),
       floatingActionButton: FloatingActionButton(
         backgroundColor: Theme.of(context).primaryColor,
-        onPressed: () async{
-          return await context.navigationService.navigateToView(
+        onPressed: () async {
+          return await context.navigationService
+              .navigateToView(
             ActivityAddView(),
-          )!.then((value) async{
-            if(value != null)
-              vm.addActivity(value);
+          )!
+              .then((value) async {
+            if (value != null) vm.addActivity(value);
           });
         },
-        child: Icon(Icons.add,color: Colors.white,),
+        child: Icon(
+          Icons.add,
+          color: Colors.white,
+        ),
       ),
       body: !vm.isBusy
           ? SingleChildScrollView(
+              controller: vm.scrollController,
               child: Column(
-                children: vm.activities!
-                    .map((activity) => ActivitySingleView(activity: activity))
-                    .toList(),
+                children: [
+                  listActivities(vm),
+                  if (vm.isLoadMoreRunning) _buildLoadMore,
+                ],
               ),
             )
           : Center(child: CircularProgressIndicator()),
+    );
+  }
+
+  Widget listActivities(ActivityViewModel viewModel) {
+    return ValueListenableBuilder<Box>(
+      valueListenable: viewModel.data.listenable(),
+      builder: (context, box, childWidget) {
+        return box.containsKey(HiveBoxKey.activities.name)
+            ? ListView.builder(
+                // controller: viewModel.scrollController,
+                primary: false,
+                shrinkWrap: true,
+                padding: EdgeInsets.only(left: 12, right: 12),
+                itemCount: box.get(HiveBoxKey.activities.name).length,
+                itemBuilder: (context, index) {
+                  return ActivitySingleView(
+                    activity: box.get(HiveBoxKey.activities.name)[index],
+                  );
+                },
+              )
+            : SizedBox();
+      },
+    );
+  }
+
+  Padding get _buildLoadMore {
+    return const Padding(
+      padding: EdgeInsets.only(top: 10, bottom: 40),
+      child: Center(
+        child: CircularProgressIndicator(),
+      ),
     );
   }
 }
--- lib/feature/views/activity/service/activity_service.dart
+++ lib/feature/views/activity/service/activity_service.dart
@@ -1,4 +1,6 @@
+import 'package:mobuni_v2/core/base/models/pagination_model/pagination_model.dart';
 import 'package:mobuni_v2/core/constants/app/api_constants.dart';
+import 'package:mobuni_v2/core/constants/app/constants.dart';
 import 'package:mobuni_v2/core/constants/enum/req_types.dart';
 import 'package:mobuni_v2/core/manager/general_manager.dart';
 import 'package:mobuni_v2/core/manager/network_manager.dart';
@@ -9,11 +11,15 @@ import 'package:stacked/stacked_annotations.dart';
 class ActivityService {
   final NetworkManager? _networkManager = GeneralManager.networkM;
 
-  Future activityGetAll({Map queryParameters = const {}}) async =>
+  Future activityGetAll({required int pageIndex,Map queryParameters = const {}}) async =>
       await _networkManager!.request(
         method: ReqTypes.get,
         path: ApiConstants.activity,
-        model: ActivityModel(),
+        model: PaginationModel<ActivityModel>(),
+        queryParameters: {
+          'PageIndex' : pageIndex,
+          'PageSize' : Constants.pageSize,
+        }
       );
 
   Future activityPost(dynamic data) async => await _networkManager!.request(

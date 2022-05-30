aba4cc5e76e0aa6739e05f3c64740652ecd81530
Bilal Özcan <bilalozcan@bixos.com>
2022-05-31 02:56:58 +0300

    question model güncellendi

--- lib/feature/models/questions/question_model.dart
+++ lib/feature/models/questions/question_model.dart
@@ -1,6 +1,7 @@
 import 'package:hive/hive.dart';
 import 'package:json_annotation/json_annotation.dart';
 import 'package:mobuni_v2/core/base/models/base_model/base_model.dart';
+import 'package:mobuni_v2/feature/models/department/department_model.dart';
 import 'package:mobuni_v2/feature/models/university/university_model.dart';
 import 'package:mobuni_v2/feature/models/user/user_model.dart';
 
@@ -27,13 +28,19 @@ class QuestionModel extends BaseModel {
   int likeCount;
   @HiveField(8)
   int? universityId;
-  //int? departmentId;
   @HiveField(9)
   UserModel? user;
   @HiveField(10)
   UniversityModel? university;
   @HiveField(11)
   bool isLiked;
+  @HiveField(12)
+  int? departmentId;
+  @HiveField(13)
+  DeaprtmentModel? department;
+  @HiveField(14)
+  bool? isUniversityStudent;
+
 
   QuestionModel({
     this.id,
@@ -48,6 +55,9 @@ class QuestionModel extends BaseModel {
     this.user,
     this.university,
     this.isLiked = false,
+    this.departmentId,
+    this.department,
+    this.isUniversityStudent,
   });
 
   factory QuestionModel.fromJson(Map<String, dynamic> json) =>
